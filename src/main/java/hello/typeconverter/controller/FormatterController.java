c0164d21f33273001a1f0f69950d6de737e4fb7c
youngrok <yr951103@gmail.com>
2025-02-10 19:17:53 +0900

    feat: 스프링이 제공하는 포맷터 예제
    - @NumberFormat 숫자 관련 형식 지정 포맷터
    - DateTimeFormat 날짜 관련 형식 지정 포맷터

---
+++ src/main/java/hello/typeconverter/controller/FormatterController.java
@@ -0,0 +1,46 @@
+package hello.typeconverter.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller
+public class FormatterController {
+
+    @GetMapping("/formatter/edit")
+    public String formatterForm(Model model) {
+        Form form = new Form();
+        form.setNumber(10000);
+        form.setLocalDateTime(LocalDateTime.now());
+
+        model.addAttribute("form", form);
+        return "formatter-form";
+    }
+
+    @PostMapping("/formatter/edit")
+    public String formatterEdit(@ModelAttribute Form form) {
+        log.info("form number = {}", form.number);
+        log.info("form localDateTime = {}", form.localDateTime);
+
+        return "formatter-view";
+    }
+
+    @Data
+    static class Form {
+
+        @NumberFormat(pattern = "###,###")
+        private Integer number;
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+    }
+}
